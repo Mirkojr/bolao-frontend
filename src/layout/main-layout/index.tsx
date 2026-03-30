fd8198e0e78e57b1db4077601d4e74ce1e599eac
mirkojr <footsbest@gmail.com>
2026-03-29 23:35:34 -0300

    ajuste nome de arquivo

--- src/layout/main-layout/index.tsx
+++ src/layout/main-layout/index.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router-dom';
-import { NavBar } from '@/shared/components/nav-bar';
-import { Footer } from '@/shared/components/footer';
+import { NavBar } from '@/shared/components/NavBar';
+import { Footer } from '@/shared/components/Footer';
 
 // Componente de layout principal que envolve as páginas com NavBar e Footer
 export function MainLayout() {
     return (
-        <div className="app-container">
+        <div className="flex flex-col min-h-screen">
             <header>
                 <NavBar />
             </header>
             
-            <main>
+            <main className="grow">
                 <Outlet />
             </main>
 
