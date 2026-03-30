9f024e2428cffa92b0a09279191712bfe0da8ca2
mirkojr <footsbest@gmail.com>
2026-03-29 23:23:32 -0300

    ano atualizavel automaticamente no footer

--- src/shared/components/nav-bar/nav-bar.tsx
+++ src/shared/components/nav-bar/nav-bar.tsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/context/AuthContext';
 
-function NavBar() {
+export const NavBar = () => {
     const { isAuthenticated, logout, user } = useAuth();
     const navigate = useNavigate();
 
